aeaf83df6b205edd7c869f2bc8c70167b41914c1
bingelis <git@bingelis.com>
2019-01-25 00:50:32 +0200

    Separate portions of calculations

--- src/LoanCalcCalculus.php
+++ src/LoanCalcCalculus.php
@@ -11,15 +11,52 @@ class LoanCalcCalculus implements LoanCalcCalculusInterface {
    * {@inheritdoc}
    */
   public function calculate($loan_amount, $interest_rate, $loan_years, $num_pmt_per_year, $loan_start, $scheduled_extra_payments = 0) {
+    $scheduled_num_of_pmt = $loan_years * $num_pmt_per_year;
+    $scheduled_monthly_payment = $this->pmt($loan_amount, $interest_rate, $num_pmt_per_year, $scheduled_num_of_pmt);
+
+    $sched_pay = $scheduled_monthly_payment;
+    $payments = [];
+    $total_early_pmt = 0;
+    $total_int = 0;
+    $actual_num_of_pmt = 0;
+
+    foreach ($this->getSchedule($loan_amount, $interest_rate, $num_pmt_per_year, $loan_start, $sched_pay, $scheduled_extra_payments) as $payment) {
+      $payments[] = $payment;
+      $actual_num_of_pmt++;
+      $total_early_pmt += $payment['extra_pay'];
+      $total_int = $payment['cum_int'];
+    }
+
+    $summary = compact(
+      'scheduled_monthly_payment',
+      'scheduled_num_of_pmt',
+      'actual_num_of_pmt',
+      'total_early_pmt',
+      'total_int'
+    );
+
+    array_walk($summary, function (&$value) {
+      $value = round($value, 2);
+    });
+
+    return compact(
+      'summary',
+      'payments'
+    );
+  }
+
+  protected function pmt($loan_amount, $interest_rate, $num_pmt_per_year, $scheduled_num_of_pmt) {
     $rate_per_pmt = ($interest_rate / 100) / $num_pmt_per_year;
 
-    $scheduled_num_of_pmt = $loan_years * $num_pmt_per_year;
     $scheduled_monthly_payment =
       ($loan_amount * pow(1 + $rate_per_pmt, $scheduled_num_of_pmt) * $rate_per_pmt) /
       (pow(1 + $rate_per_pmt, $scheduled_num_of_pmt) - 1);
 
-    $sched_pay = $scheduled_monthly_payment;
-    $beg_bal = $loan_amount;
+    return $scheduled_monthly_payment;
+  }
+
+  protected function getSchedule($loan_amount, $interest_rate, $num_pmt_per_year, $loan_start, $sched_pay, $scheduled_extra_payments = 0) {
+    $beg_bal = (int) $loan_amount;
     $pay_num = 1;
     $cum_int = 0;
     $total_early_pmt = 0;
@@ -32,7 +69,7 @@ class LoanCalcCalculus implements LoanCalcCalculusInterface {
 
       // Extra Payment.
       if ($sched_pay + $scheduled_extra_payments < $beg_bal) {
-        $extra_pay = $scheduled_extra_payments;
+        $extra_pay = (int) $scheduled_extra_payments;
       }
       elseif ($beg_bal - $sched_pay > 0) {
         $extra_pay = $beg_bal - $sched_pay;
@@ -50,6 +87,7 @@ class LoanCalcCalculus implements LoanCalcCalculusInterface {
       }
 
       $total_early_pmt += $extra_pay;
+      $rate_per_pmt = ($interest_rate / 100) / $num_pmt_per_year;
       $int = $beg_bal * $rate_per_pmt;
       $princ = $total_pay - $int;
       $cum_int += $int;
@@ -85,25 +123,7 @@ class LoanCalcCalculus implements LoanCalcCalculusInterface {
       $beg_bal = $end_bal;
     }
 
-    $actual_num_of_pmt = count($payments);
-    $total_int = $cum_int;
-
-    $summary = compact(
-      'scheduled_monthly_payment',
-      'scheduled_num_of_pmt',
-      'actual_num_of_pmt',
-      'total_early_pmt',
-      'total_int'
-    );
-
-    array_walk($summary, function (&$value) {
-      $value = round($value, 2);
-    });
-
-    return compact(
-      'summary',
-      'payments'
-    );
+    return $payments;
   }
 
 }
